91de5b16006c3c5d120429e7b1547b5e105c2122
nicofirmansyah <nicofirmansyah057@gmail.com>
2022-04-07 01:35:22 +0700

    commit2

--- app/Http/Controllers/CobaController.php
+++ app/Http/Controllers/CobaController.php
@@ -13,6 +13,18 @@ class CobaController extends Controller
 
     public function urutan($ke)
     {
-            return view ('urutan', ['ke' => $ke]);
+
+        $numbers = [
+                ['ke' => 1, 'nomor' => 20],
+                ['ke' => 2, 'nomor' => 30],
+                ['ke' => 3, 'nomor' => 40],
+        ];
+
+
+            return view ('urutan', compact ('numbers'));
+    }
+    public function coba($ke)
+    {
+            return view ('coba', ['ke' => $ke]);
     }
 }
